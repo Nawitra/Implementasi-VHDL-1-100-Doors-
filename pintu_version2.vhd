0b1031309b7f87cc83c2a2c75e0b02071f01d24a
Natalia Kristian <51152809+Dronex-Reboot@users.noreply.github.com>
2020-04-28 15:52:50 +0700

    Update pintu_version2.vhd

--- pintu_version2.vhd
+++ pintu_version2.vhd
@@ -1,20 +1,20 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
-ENTITY doors IS
+ENTITY PINTU IS
 	
 	PORT (	
 				CLK			: IN STD_LOGIC;
 				RST			: IN STD_LOGIC;
 				OUTPUT		: INOUT STD_LOGIC_VECTOR(1 TO 100) := (OTHERS => '0')
 	);
-END doors;
+END PINTU;
 
-ARCHITECTURE BEHAVIOUR OF doors IS
+ARCHITECTURE BEHAVIOUR OF PINTU IS
 	TYPE STATE IS (TERBUKA, TERTUTUP);
 	SIGNAL PRESENT			: STATE;
 	SIGNAL NEXT_STATE		: STATE;
-	SIGNAL PINTU 			: INTEGER RANGE 1 TO 102;
+	SIGNAL PINTU 			: INTEGER RANGE 1 TO 100;
 	SIGNAL KELIPATAN		: INTEGER RANGE 1 TO 6;
 	
 BEGIN
@@ -26,18 +26,28 @@ BEGIN
 				OUTPUT		<= (OTHERS => '0');
 			END IF;
 			
-			IF (KELIPATAN <= 3) THEN
-				IF (PINTU < 99) THEN
+					
+			IF (RISING_EDGE(CLK)) THEN	
+			
+			--Cek apakah kelipatannya adalah 3
+			IF (KELIPATAN = 3) THEN
+				IF (PINTU < 99) THEN --Maksimal kelipatan 3 adalah 99
 					OUTPUT(PINTU) <= NOT(OUTPUT(PINTU));
 					PINTU <= PINTU + KELIPATAN;
-					END IF;
+					
+				ELSIF (KELIPATAN < 6) THEN
+					PINTU <= KELIPATAN + 1;
+					KELIPATAN <= KELIPATAN + 1;
+						
+				ELSE
+					PINTU <= 1;
+					KELIPATAN <= 1;
 				END IF;
 					
-			IF (RISING_EDGE(CLK)) THEN
-				IF (PINTU < 100) THEN
+				ELSIF (PINTU < 100) THEN
 					OUTPUT(PINTU) <= NOT(OUTPUT(PINTU));
 					PINTU <= PINTU + KELIPATAN;
-					
+				
 					
 					--Cek state dari pintu saat ini
 					IF ((PINTU MOD KELIPATAN) = 0) THEN
@@ -46,7 +56,8 @@ BEGIN
 						ELSIF (PRESENT = TERTUTUP) THEN
 							NEXT_STATE <= TERBUKA;
 						END IF;
-					END IF;
+					END IF;				
+					
 				
 					
 				ELSIF (KELIPATAN < 6) THEN
@@ -56,6 +67,7 @@ BEGIN
 					PINTU <= 1;
 					KELIPATAN <= 1;
 				END IF;
+			
 				
 				--Supaya di luar clock edge tetap terdapat nilai
 			ELSE
